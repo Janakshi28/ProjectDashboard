fc3f3f64dbd2a974b88caed8613f12bb2e977f93
Ranula <ranulapbl@gmail.com>
2017-07-26 12:13:04 +0530

    Team satisfaction data

---
+++ Seranet.ProjectDashBoard/ProDashBoard.Model/CD_TeamSatisfactionData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProDashBoard.Model
+{
+    public class CD_TeamSatisfactionData
+    {
+        public int Year { get; set; }
+        public String Quarter { get; set; }
+        public String Rating { get; set; }
+        public string Completion { get; set; }
+    }
+}
